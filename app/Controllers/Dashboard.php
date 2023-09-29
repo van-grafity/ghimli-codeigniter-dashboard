424eaebe675925073db003788decaadf3cc928ec
Ridwan Pranata (GLA) <ridwanpranata10@gmail.com>
2023-09-29 10:45:00 +0700

    Fresh Install and Add template and configuration

---
+++ app/Controllers/Dashboard.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controllers;
+
+class Dashboard extends BaseController
+{
+    public function index(): string
+    {
+        $data = [
+            'url_machine_app' => getenv('URL_MACHINE_APP'),
+            'url_cutting_app' => getenv('URL_CUTTING_APP'),
+            'url_sewing_app' => getenv('URL_SEWING_APP'),
+            'url_packing_app' => getenv('URL_PACKING_APP'),
+            'url_compliance_app' => getenv('URL_COMPLIANCE_APP'),
+        ];
+        
+        return view('ghimli-dashboard', $data);
+    }
+}
---
+++ app/Views/ghimli-dashboard.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <!--[if IE]><meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"><![endif]-->
+        <!--
+        Zoom Template 
+        http://www.templatemo.com/tm-414-zoom
+        -->
+        <title>Ghim Li App Module</title>
+        <meta name="description" content="">
+        <meta name="viewport" content="width=device-width">
+        
+        <!-- Google Web Font Embed -->
+        <link href='http://fonts.googleapis.com/css?family=Raleway:400,600,500,300,700' rel='stylesheet' type='text/css'>
+        
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- <link rel="stylesheet" href="css/font-awesome.min.css"> -->
+        <link rel="stylesheet" href="css/templatemo_main.css">
+    </head>
+    <body>
+        <div id="main-wrapper">
+            <!--[if lt IE 7]>
+                <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+            <![endif]-->
+
+            <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12 text-center templatemo-logo margin-top-20">
+                <h1 class="templatemo-site-title">
+                    <a href="javascript:void(0)">Welcome to Ghim Li Indonesia Apps</a>
+                </h1>
+            </div>
+
+            <div class="image-section">
+                <div class="image-container">
+                    <img src="images/dashboard-background.jpg" id="menu-img" class="main-img inactive" alt="Ghimli Dashboard background">
+                    <!-- <img src="images/zoom-bg-1.jpg" id="menu-img" class="main-img inactive" alt="Ghimli Dashboard background"> -->
+                </div>
+            </div>
+
+            <div class="container">
+                <div class="col-xs-12 col-sm-12 col-md-8 col-lg-8 col-md-offset-2 col-lg-offset-2 templatemo-content-wrapper">
+                    <div class="templatemo-content">
+                        
+                        <section id="menu-section" class="active">
+                            <div class="row">
+                                <div class="col-xs-6 col-sm-4 col-lg-4 margin-bottom-20">
+                                    <a href="<?= $url_cutting_app ?>" class="change-section">
+                                        <div class="black-bg btn-menu">
+                                            <i class="fa fa-solid fa-ticket"></i>
+                                            <h2>Cutting Ticket App</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-xs-6 col-sm-4 col-lg-4 margin-bottom-20">
+                                    <a href="../sewing-app/public" class="change-section">
+                                        <div class="black-bg btn-menu">
+                                            <i class="fa fa-solid fa-shirt"></i>
+                                            <h2>Sewing App</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-xs-6 col-sm-4 col-lg-4 margin-bottom-20">
+                                    <a href="<?= $url_packing_app ?>" class="change-section">
+                                        <div class="black-bg btn-menu">
+                                            <i class="fa fas fa-box-open"></i>
+                                            <h2>Scan & Pack</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-xs-6 col-sm-4 col-lg-4 margin-bottom-20">
+                                    <a href="<?= $url_compliance_app ?>" class="change-section">
+                                        <div class="black-bg btn-menu">
+                                            <i class="fa fa-solid fa-scale-balanced"></i>
+                                            <h2>Compliance App</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="col-xs-6 col-sm-4 col-lg-4 margin-bottom-20">
+                                    <a href="<?= $url_machine_app ?>" class="change-section">
+                                        <div class="black-bg btn-menu">
+                                            <i class="fa fa-solid fa-cash-register"></i>
+                                            <!-- <i class="fas fa-cash-register"></i> -->
+                                            <h2>Machine App</h2>
+                                        </div>
+                                    </a>
+                                </div>
+                            </div>
+                        </section><!-- /.menu-section -->    
+                        
+                    </div><!-- /.templatemo-content -->  
+                </div><!-- /.templatemo-content-wrapper --> 
+            </div><!-- /.row --> 
+
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12 footer">
+                    <p class="footer-text">Copyright &copy; 2023 Ghim Li Indonesia</p>
+                </div><!-- /.footer --> 
+            </div>
+
+		</div><!-- /#main-wrapper -->
+        
+        <div id="preloader">
+            <div id="status">&nbsp;</div>
+        </div><!-- /#preloader -->
+        
+        <script src="js/jquery.js"></script>
+        <script src="js/jquery-ui.min.js"></script>
+        <script src="js/jquery.backstretch.js"></script>
+        <script src="js/templatemo_script.js"></script>
+        <script src="https://kit.fontawesome.com/a703abfe53.js" crossorigin="anonymous"></script>
+
+    </body> 
+</html>
